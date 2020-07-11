fe8e9336fa06e1e983599e53086764bc3c56e9b5
Igor Benedet <benedetigor@gmail.com>
2020-07-11 15:52:57 -0300

    funcao para mostrar o campo

--- main.cpp
+++ main.cpp
@@ -2,20 +2,35 @@
 #include <time.h>
 using namespace std;
 
+//mostra o campo do jogo na tela
+void showField(int linhas, int colunas){
+    //- = campo vazio
+    //x = bomba
+    cout << "Campo: " << endl;
+    for(int i=0;i<linhas;i++){
+        for(int j=0;j<colunas;j++){
+            cout << "- ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
   int i,j,bomb,bomb2;
+
   cout<<"Insira o numero de linhas"<<endl;
   cin>>i;
   cout<<"Insira o numero de colunas"<<endl;
   cin>>j;
   if (i==2&&j==2){
-  bomb=2;
+    bomb=2;
+  }else if (i>2 && j>2){
+    while(bomb>2){
+        cout<<"Insira o numero de bombas"<<endl;
+        cin>>bomb2;
+        bomb=bomb+bomb2;
+    }
+
   }
-  else if (i>2 && j>2){
-  while(bomb>2){
-   cout<<"Insira o numero de bombas"<<endl;
-   cin>>bomb2;
-   bomb=bomb+bomb2;
-  }
-}
+ showField(i, j);
 }
