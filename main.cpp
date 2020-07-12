4377795e6084490e1c5bc4db7a438f90a10af091
Igor Benedet <benedetigor@gmail.com>
2020-07-12 15:05:00 -0300

    aa

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <time.h>
+#include <vector>
+
 using namespace std;
 
 //mostra o campo do jogo na tela
 void showField(int linhas, int colunas){
     //- = campo vazio
     //x = bomba
+
+    char linhas_letters[26] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+    char colunas_indices[26] = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+
     cout << "Campo: " << endl;
     for(int i=0;i<linhas;i++){
         for(int j=0;j<colunas;j++){
@@ -14,6 +20,8 @@ void showField(int linhas, int colunas){
         cout << endl;
     }
 }
+
+
 
 int main(){
   int i,j,bomb,bomb2;
